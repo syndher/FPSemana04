10f96abc5e1b2e747599cf7fe15ab4a7fe9dfedf
Miguel Filipe <miguelfilipe.dev@gmail.com>
2024-11-29 13:51:43 +0000

    tweaked read_json.py

---
+++ read_json.py
@@ -0,0 +1,14 @@
+import json
+
+
+try:
+    path = input()
+    file = open(path, "rt", encoding="utf-8")
+    data = json.load(file)
+    print(data)
+    file.close()
+except(FileNotFoundError):
+    print("Ocorreu um erro!")
+finally:
+    print("Processo concluído")
+ 
